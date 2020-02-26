42cd57590e4abb058ae317d425e4c7b261b0a0f9
System Administrator <root@veneroso.local>
2020-02-26 14:03:22 -0300

    JS DE modificado

--- EU-MEA/Translated Locales/DE/js/animation.js
+++ EU-MEA/Translated Locales/DE/js/animation.js
@@ -1,176 +1,193 @@
 // Animation desktop
-$(document).ready(function(){
-    $('.box-hover-1').hover(function(){
+$(document).ready(function() {
+    $('.box-hover-1').hover(function() {
 
-        $('.ls-head').hide();
-        $('.ls-subhead').hide();
-        $('.cards-intro').hide();
-        $('.links-bottom').hide();
+            $('.ls-head').hide();
+            $('.ls-subhead').hide();
+            $('.cards-intro').hide();
+            $('.links-bottom').hide();
 
-        $('.cards-intro').fadeIn(1000);
-        $('.links-bottom').fadeIn(1300);
+            $('.cards-intro').fadeIn(1000);
+            $('.links-bottom').fadeIn(1300);
 
-        $('.ls-head').text("Strategy and risk management");
-        $('.ls-head').fadeIn(1000);
-        $('.ls-subhead').text("Advance your cybersecurity strategy and manage risks with a partner that understands your business");
-        $('.ls-subhead').fadeIn(1300);
+            var1 = "Strategie und Risikomanagement";
+            $('.ls-head').text(var1);
+            $('.ls-head').fadeIn(1000);
+            var2 = "Entwickeln Sie Ihre Strategie für die Cybersicherheit weiter und halten Sie die Risiken unter Kontrolle – mit einem Partner, der Ihr Business versteht. ";
+            $('.ls-subhead').text(var2);
+            $('.ls-subhead').fadeIn(1300);
 
-    },
-    function(){
-        $('.ls-head').text("Security Solutions");
-        $('.ls-subhead').text("Discover solutions that unite defenses and unlock innovation with a partner that understands your business");
-    })
+        },
+        function() {
+            var3 = "Sicherheitslösungen";
+            $('.ls-head').text(va3);
+            var4 = "Lernen Sie Lösungen kennen, die Abwehrmaßnahmen verknüpfen und Innovationen erschließen – mit einem Partner, der Ihr Business versteht. ";
+            $('.ls-subhead').text(var4);
+        })
 
-    $('.box-hover-2').hover(function(){
+    $('.box-hover-2').hover(function() {
 
-      $('.ls-head').hide();
-      $('.ls-subhead').hide();
-      $('.cards-intro').hide();
-      $('.links-bottom').hide();
+            $('.ls-head').hide();
+            $('.ls-subhead').hide();
+            $('.cards-intro').hide();
+            $('.links-bottom').hide();
 
-      $('.cards-intro').fadeIn(1000);
-      $('.links-bottom').fadeIn(1300);
+            $('.cards-intro').fadeIn(1000);
+            $('.links-bottom').fadeIn(1300);
 
-      $('.ls-head').text("Threat management");
-      $('.ls-head').fadeIn(1000);
-      $('.ls-subhead').text("Unite your defenses to simplify and strengthen threat detection, analysis and response");
-      $('.ls-subhead').fadeIn(1300);
+            var5 = "Management von Sicherheitsbedrohungen";
+            $('.ls-head').text(var5);
+            $('.ls-head').fadeIn(1000);
+            var6 = "Verknüpfen Sie Ihre Sicherheitsmaßnahmen, um die Erkennung und Analyse von Sicherheitsbedrohungen sowie die Reaktion darauf zu vereinfachen und effektiver zu machen. ";
+            $('.ls-subhead').text(var6);
+            $('.ls-subhead').fadeIn(1300);
 
-  },
-  function(){
-      $('.ls-head').text("Security Solutions");
-      $('.ls-subhead').text("Discover solutions that unite defenses and unlock innovation with a partner that understands your business");
-  })
+        },
+        function() {
+            var7 = "Sicherheitslösungen";
+            $('.ls-head').text(var7);
+            var8 = "Lernen Sie Lösungen kennen, die Abwehrmaßnahmen verknüpfen und Innovationen erschließen – mit einem Partner, der Ihr Business versteht. ";
+            $('.ls-subhead').text(var8);
+        })
 
-  $('.box-hover-3').hover(function(){
+    $('.box-hover-3').hover(function() {
 
-    $('.ls-head').hide();
-    $('.ls-subhead').hide();
-    $('.cards-intro').hide();
-    $('.links-bottom').hide();
+            $('.ls-head').hide();
+            $('.ls-subhead').hide();
+            $('.cards-intro').hide();
+            $('.links-bottom').hide();
 
-    $('.cards-intro').fadeIn(1000);
-    $('.links-bottom').fadeIn(1300);
+            $('.cards-intro').fadeIn(1000);
+            $('.links-bottom').fadeIn(1300);
 
-    $('.ls-head').text("Digital trust");
-    $('.ls-head').fadeIn(1000);
-    $('.ls-subhead').text("Unlock innovation by developing trusted user relationships and protecting data that powers your business");
-    $('.ls-subhead').fadeIn(1300);
+            var9 = "Vertrauen in der digitalen Welt";
+            $('.ls-head').text(var9);
+            $('.ls-head').fadeIn(1000);
+            var10 = "Erschließen Sie Innovationen, indem Sie vertrauensvolle Beziehungen mit Benutzern aufbauen und die Daten schützen, die das Fundament Ihres Unternehmens bilden. ";
+            $('.ls-subhead').text(var10);
+            $('.ls-subhead').fadeIn(1300);
 
-    },
-    function(){
-        $('.ls-head').text("Security Solutions");
-        $('.ls-subhead').text("Discover solutions that unite defenses and unlock innovation with a partner that understands your business");
-    })
+        },
+        function() {
+            var11 = "Sicherheitslösungen";
+            $('.ls-head').text(var11);
+            var12 = "Lernen Sie Lösungen kennen, die Abwehrmaßnahmen verknüpfen und Innovationen erschließen – mit einem Partner, der Ihr Business versteht. ";
+            $('.ls-subhead').text(var12);
+        })
 
-    $('.box-hover-4').hover(function(){
+    $('.box-hover-4').hover(function() {
 
-      $('.ls-head').hide();
-      $('.ls-subhead').hide();
-      $('.cards-intro').hide();
-      $('.links-bottom').hide();
-  
-      $('.cards-intro').fadeIn(1000);
-      $('.links-bottom').fadeIn(1300);
-  
-      $('.ls-head').text("Cloud security");
-      $('.ls-head').fadeIn(1000);
-      $('.ls-subhead').text("Move confidently to the cloud by integrating security into every phase of your journey");
-      $('.ls-subhead').fadeIn(1300);
-  
-      },
-      function(){
-          $('.ls-head').text("Security Solutions");
-          $('.ls-subhead').text("Discover solutions that unite defenses and unlock innovation with a partner that understands your business");
-      })    
-})  
-   
-    
- 
-$(document).ready(function(){          
+            $('.ls-head').hide();
+            $('.ls-subhead').hide();
+            $('.cards-intro').hide();
+            $('.links-bottom').hide();
+
+            $('.cards-intro').fadeIn(1000);
+            $('.links-bottom').fadeIn(1300);
+
+            var13 = "Cloud-Sicherheit";
+            $('.ls-head').text(var13);
+            $('.ls-head').fadeIn(1000);
+            var14 = "Steigen Sie ohne Bedenken auf die Cloud um, indem Sie Sicherheit in jede Phase der Umstellung integrieren. ";
+            $('.ls-subhead').text(var14);
+            $('.ls-subhead').fadeIn(1300);
+
+        },
+        var15 = "Sicherheitslösungen";
+
+        function() {
+            $('.ls-head').text(var15);
+            var16 = "Lernen Sie Lösungen kennen, die Abwehrmaßnahmen verknüpfen und Innovationen erschließen – mit einem Partner, der Ihr Business versteht. ";
+            $('.ls-subhead').text(var16);
+        })
+})
+
+
+
+$(document).ready(function() {
 
     //Animation Mobile
 
-    $( ".box-sm-1" ).click(function() {
+    $(".box-sm-1").click(function() {
         // console.warn('fui clicado')
-        $( ".box-sm-1" ).addClass( "box-expand" );
-        $( ".box-item-default" ).hide();
+        $(".box-sm-1").addClass("box-expand");
+        $(".box-item-default").hide();
         $('.close-btn').fadeIn(1500);
         $('.box-item-expand-1').fadeIn(2000);
-        $( ".box-sm-2" ).hide();
-        $( ".box-sm-3" ).hide();
-        $( ".box-sm-4" ).hide();
-      });
-      
-      $( ".box-sm-2" ).click(function() {
-        $( ".box-sm-2" ).addClass( "box-expand" );
-        $( ".box-item-default" ).hide();
+        $(".box-sm-2").hide();
+        $(".box-sm-3").hide();
+        $(".box-sm-4").hide();
+    });
+
+    $(".box-sm-2").click(function() {
+        $(".box-sm-2").addClass("box-expand");
+        $(".box-item-default").hide();
         $('.close-btn').fadeIn(1500);
         $('.box-item-expand-2').fadeIn(2000);
-        $( ".box-sm-1" ).hide();
-        $( ".box-sm-3" ).hide();
-        $( ".box-sm-4" ).hide();
-      });
+        $(".box-sm-1").hide();
+        $(".box-sm-3").hide();
+        $(".box-sm-4").hide();
+    });
 
-      $( ".box-sm-3" ).click(function() {
-        $( ".box-sm-3" ).addClass( "box-expand" );
-        $( ".box-item-default" ).hide();
+    $(".box-sm-3").click(function() {
+        $(".box-sm-3").addClass("box-expand");
+        $(".box-item-default").hide();
         $('.close-btn').fadeIn(1500);
         $('.box-item-expand-3').fadeIn(2000);
-        $( ".box-sm-2" ).hide();
-        $( ".box-sm-1" ).hide();
-        $( ".box-sm-4" ).hide();
-      });
+        $(".box-sm-2").hide();
+        $(".box-sm-1").hide();
+        $(".box-sm-4").hide();
+    });
 
-      $( ".box-sm-4" ).click(function() {
-        $( ".box-sm-4" ).addClass( "box-expand" );
-        $( ".box-item-default" ).hide();
+    $(".box-sm-4").click(function() {
+        $(".box-sm-4").addClass("box-expand");
+        $(".box-item-default").hide();
         $('.close-btn').fadeIn(1500);
         $('.box-item-expand-4').fadeIn(2000);
-        $( ".box-sm-2" ).hide();
-        $( ".box-sm-3" ).hide();
-        $( ".box-sm-1" ).hide();
-      });   
+        $(".box-sm-2").hide();
+        $(".box-sm-3").hide();
+        $(".box-sm-1").hide();
+    });
 
-      $( ".close-btn" ).click(function() {
-        if($( ".box-sm-1" ).hasClass( "box-expand" )){
-          $( ".box-sm-1" ).removeClass( "box-expand" );
-          $( ".close-btn" ).hide();
-          $('.box-item-expand-1').hide();
-          $( ".box-sm-2" ).show();
-          $( ".box-sm-3" ).show();
-          $( ".box-sm-4" ).show();
-          $( ".box-item-default" ).show();
+    $(".close-btn").click(function() {
+        if ($(".box-sm-1").hasClass("box-expand")) {
+            $(".box-sm-1").removeClass("box-expand");
+            $(".close-btn").hide();
+            $('.box-item-expand-1').hide();
+            $(".box-sm-2").show();
+            $(".box-sm-3").show();
+            $(".box-sm-4").show();
+            $(".box-item-default").show();
         }
 
-        if($( ".box-sm-2" ).hasClass( "box-expand" )){
-          $( ".box-sm-2" ).removeClass( "box-expand" );
-          $( ".close-btn" ).hide();
-          $('.box-item-expand-2').hide();
-          $( ".box-sm-1" ).show();
-          $( ".box-sm-3" ).show();
-          $( ".box-sm-4" ).show();
-          $( ".box-item-default" ).show();
+        if ($(".box-sm-2").hasClass("box-expand")) {
+            $(".box-sm-2").removeClass("box-expand");
+            $(".close-btn").hide();
+            $('.box-item-expand-2').hide();
+            $(".box-sm-1").show();
+            $(".box-sm-3").show();
+            $(".box-sm-4").show();
+            $(".box-item-default").show();
         }
 
-        if($( ".box-sm-3" ).hasClass( "box-expand" )){
-          $( ".box-sm-3" ).removeClass( "box-expand" );
-          $( ".close-btn" ).hide();
-          $('.box-item-expand-3').hide();
-          $( ".box-sm-1" ).show();
-          $( ".box-sm-2" ).show();
-          $( ".box-sm-4" ).show();
-          $( ".box-item-default" ).show();
+        if ($(".box-sm-3").hasClass("box-expand")) {
+            $(".box-sm-3").removeClass("box-expand");
+            $(".close-btn").hide();
+            $('.box-item-expand-3').hide();
+            $(".box-sm-1").show();
+            $(".box-sm-2").show();
+            $(".box-sm-4").show();
+            $(".box-item-default").show();
         }
 
-        if($( ".box-sm-4" ).hasClass( "box-expand" )){
-          $( ".box-sm-4" ).removeClass( "box-expand" );
-          $( ".close-btn" ).hide();
-          $('.box-item-expand-4').hide();
-          $( ".box-sm-1" ).show();
-          $( ".box-sm-2" ).show();
-          $( ".box-sm-3" ).show();
-          $( ".box-item-default" ).show();
+        if ($(".box-sm-4").hasClass("box-expand")) {
+            $(".box-sm-4").removeClass("box-expand");
+            $(".close-btn").hide();
+            $('.box-item-expand-4').hide();
+            $(".box-sm-1").show();
+            $(".box-sm-2").show();
+            $(".box-sm-3").show();
+            $(".box-item-default").show();
         }
 
 
@@ -182,24 +199,8 @@ $(document).ready(function(){
         // $( ".box-sm-3" ).show();
         // $( ".box-sm-4" ).show();
         // console.warn("rodou btn click", $( ".box-sm-1" ));
-      });      
-      
-      
-    
+    });
+
+
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

930c9710a0b4a247840ba591624091610db583c6
System Administrator <root@veneroso.br.ibm.com>
2020-03-06 10:45:52 -0300

    TR atualizada

--- EU-MEA/Translated Locales/TR/js/animation.js
+++ EU-MEA/Translated Locales/TR/js/animation.js
@@ -10,15 +10,19 @@ $(document).ready(function() {
             $('.cards-intro').fadeIn(1000);
             $('.links-bottom').fadeIn(1300);
 
-            $('.ls-head').text("Strateji ve risk yönetimi");
+            var1 = "Strateji ve risk yönetimi";
+            $('.ls-head').text(var1);
             $('.ls-head').fadeIn(1000);
-            $('.ls-subhead').text("İşinizi anlayan bir ortakla, siber güvenlik stratejinizi geliştirin ve riskleri yönetin");
+            var2 = "İşinizi anlayan bir ortakla, siber güvenlik stratejinizi geliştirin ve riskleri yönetin";
+            $('.ls-subhead').text(var2);
             $('.ls-subhead').fadeIn(1300);
 
         },
         function() {
-            $('.ls-head').text("Güvenlik Çözümleri");
-            $('.ls-subhead').text("İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin");
+            var3 = "Güvenlik Çözümleri";
+            $('.ls-head').text(va3);
+            var4 = "İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin";
+            $('.ls-subhead').text(var4);
         })
 
     $('.box-hover-2').hover(function() {
@@ -31,15 +35,19 @@ $(document).ready(function() {
             $('.cards-intro').fadeIn(1000);
             $('.links-bottom').fadeIn(1300);
 
-            $('.ls-head').text("Tehdit yönetimi");
+            var5 = "Tehdit yönetimi";
+            $('.ls-head').text(var5);
             $('.ls-head').fadeIn(1000);
-            $('.ls-subhead').text("Tehdit saptama, analiz ve müdahaleyi basitleştirmek ve güçlendirmek için savunma mekanizmalarınızı birleştirin");
+            var6 = "Tehdit saptama, analiz ve müdahale süreçlerini basitleştirmek ve güçlendirmek için savunma mekanizmalarınızı birleştirin";
+            $('.ls-subhead').text(var6);
             $('.ls-subhead').fadeIn(1300);
 
         },
         function() {
-            $('.ls-head').text("Güvenlik Çözümleri");
-            $('.ls-subhead').text("İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin");
+            var7 = "Güvenlik Çözümleri";
+            $('.ls-head').text(var7);
+            var8 = "İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin";
+            $('.ls-subhead').text(var8);
         })
 
     $('.box-hover-3').hover(function() {
@@ -52,15 +60,19 @@ $(document).ready(function() {
             $('.cards-intro').fadeIn(1000);
             $('.links-bottom').fadeIn(1300);
 
-            $('.ls-head').text("Dijital güven");
+            var9 = "Dijital güven";
+            $('.ls-head').text(var9);
             $('.ls-head').fadeIn(1000);
-            $('.ls-subhead').text("Güvenilir kullanıcı ilişkileri geliştirerek ve işinize güç katan verileri koruyarak inovasyona olanak sağlayın");
+            var10 = "Güvenilir kullanıcı ilişkileri geliştirerek ve işinize güç katan verileri koruyarak inovasyona olanak sağlayın";
+            $('.ls-subhead').text(var10);
             $('.ls-subhead').fadeIn(1300);
 
         },
         function() {
-            $('.ls-head').text("Güvenlik Çözümleri");
-            $('.ls-subhead').text("İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin");
+            var11 = "Güvenlik Çözümleri";
+            $('.ls-head').text(var11);
+            var12 = "İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin";
+            $('.ls-subhead').text(var12);
         })
 
     $('.box-hover-4').hover(function() {
@@ -73,15 +85,20 @@ $(document).ready(function() {
             $('.cards-intro').fadeIn(1000);
             $('.links-bottom').fadeIn(1300);
 
-            $('.ls-head').text("Bulut güvenliği");
+            var13 = "Bulut güvenliği";
+            $('.ls-head').text(var13);
             $('.ls-head').fadeIn(1000);
-            $('.ls-subhead').text("Güvenliği yolculuğunuzun her aşamasına entegre ederek buluta güvenle geçin");
+            var14 = "Buluta geçiş yolculuğunuzun her aşamasına güvenliği entegre ederek buluta güvenle geçin ";
+            $('.ls-subhead').text(var14);
             $('.ls-subhead').fadeIn(1300);
 
         },
+        var15 = "Güvenlik Çözümleri";
+
         function() {
-            $('.ls-head').text("Güvenlik Çözümleri");
-            $('.ls-subhead').text("İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin");
+            $('.ls-head').text(var15);
+            var16 = "İşinizi anlayan bir ortakla, savunma mekanizmalarını birleştiren ve inovasyona olanak sağlayan güvenlik çözümlerini keşfedin";
+            $('.ls-subhead').text(var16);
         })
 })
 
